d35a414ed91f11aca8452e2d94f6891c0a3e6434
Nathan Kinkead <natekinkead@gmail.com>
2026-04-18 09:38:09 -0700

    custom domain

--- scripts/ghpages-deploy.mjs
+++ scripts/ghpages-deploy.mjs
@@ -5,6 +5,7 @@ ghpages.publish(
     {
         branch: process.env.DEPLOY_BRANCH || 'gh-pages',
         repo: process.env.DEPLOY_REPO_URL || undefined,
+        cname: process.env.DEPLOY_CNAME || 'crm.campercapital.net',
     },
     function (err) {
         if (err) {
